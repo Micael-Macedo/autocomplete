87f235b3b62e31fb05046ca2dd5a2c6fb95b5304
Micael Macedo <micaelm2009@hotmail.com>
2024-12-04 00:19:53 -0300

    feat: add project

---
+++ src/app/autocomplete/autocomplete.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {AsyncPipe} from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+@Component({
+  selector: 'app-autocomplete',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
+  templateUrl: './autocomplete.component.html',
+  styleUrl: './autocomplete.component.scss'
+})
+export class AutocompleteComponent implements OnInit {
+  myControl = new FormControl({ value: '', disabled: false });
+  item2 = new FormControl({ value: '', disabled: true });
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions!: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  mudarEstado(){
+    this.item2.enable()
+  }
+}
